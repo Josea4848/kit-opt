187397bca503a7bd25388adfb1521b12fbbbd253
José Alves <josealves3416@gmail.com>
2024-04-08 23:45:04 -0300

    bugs fixed: swap, reinsertion

--- GILS-RVND-TSP/leitor-instancias/src/main.cpp
+++ GILS-RVND-TSP/leitor-instancias/src/main.cpp
@@ -135,12 +135,23 @@ bool bestImprovementSwap(Solucao *s, Data& data) {
         int vi_prev = s->sequencia[i - 1]; //previous
         int vi_next = s->sequencia[i + 1]; //next
 
-        for(int j = i + 1; j < s->sequencia.size() - 1; j++) {
+        for(int j = i + 1; j < s->sequencia.size() - 1; j++) {        
             int vj = s->sequencia[j];
             int vj_next = s->sequencia[j + 1];
             int vj_prev = s->sequencia[j - 1];
+            double delta = 0;
+           
+            if(j == i + 1) {
+                delta = data.getDistance(vj, vi_prev)  + data.getDistance(vi, vj_next) - data.getDistance(vi, vi_prev) - data.getDistance(vj, vj_next);    
+            }
+            else { 
             //Custo Novo - anterior nos entre nós que podem ser alterados
-            double delta =  - data.getDistance(vi_prev,vi) - data.getDistance(vi,vi_next) + data.getDistance(vi_prev,vj) + data.getDistance(vj, vi_next) - data.getDistance(vj_prev, vj) - data.getDistance(vj, vj_next) + data.getDistance(vj_prev,vi) + data.getDistance(vi, vj_next);
+                delta =  data.getDistance(vi, vj_prev) + data.getDistance(vi, vj_next) + data.getDistance(vj, vi_prev) +  data.getDistance(vj, vi_next) - data.getDistance(vi, vi_prev) - data.getDistance(vi, vi_next) - data.getDistance(vj, vj_prev) - data.getDistance(vj, vj_next);
+
+                cout << data.getDistance(vi, vj_prev) << " " <<  data.getDistance(vi, vj_next) << " " << data.getDistance(vj, vi_prev) << " " <<  data.getDistance(vj, vi_next) << " " << data.getDistance(vi, vi_prev)<< " " << data.getDistance(vi, vi_next)<< " " << data.getDistance(vj, vj_prev)<< " " << data.getDistance(vj, vj_next) << endl;
+            }
+                
+            
             //custo menor
             if(delta < bestDelta) {
                 bestDelta = delta;
@@ -194,39 +205,44 @@ bool bestImprovement2Opt(Solucao *s, Data &data) {
     return false;
 }
 
-bool reinsertion(Solucao *s, Data &data) {
+bool reinsertion(Solucao *s , Data &data, int n) {
     double bestDelta = 0;
     int best_i, best_j;
 
-    for(int i = 1; i < s->sequencia.size() - 1; i++) {
+    for(int i = 1; i < s->sequencia.size() - n; i++) {
         int vi = s->sequencia[i];
-        int vi_next = s->sequencia[i + 1];
+        int vi_next = s->sequencia[i + n];
         int vi_prev = s->sequencia[i - 1];
-        //cout << "[" << vi_prev << ", " << vi << ", " << vi_next << "]\n";
-        for(int j = i + 1; j < s->sequencia.size() - 1; j++) {
-            //cout << "(" << s->sequencia[i] << ", " << s->sequencia[j] << ")\n";
-            //if (j == i) 
-                //continue;
-            //possível antigo nó de posição j
+        
+        for(int j = 1; j < s->sequencia.size() - 1; j++) {
+            if(i == j)
+                continue;
+
+            int vj_prev = s->sequencia[j - 1];
             int vj = s->sequencia[j];
-            int vj_next = s->sequencia[j+1];
+            int vj_next = s->sequencia[j + 1];
+            double delta;
 
-            double delta = data.getDistance(vi_prev, vi_next) + data.getDistance(vj, vi) + data.getDistance(vi, vj_next) - data.getDistance(vi_prev, vi) - data.getDistance(vi, vi_next) - data.getDistance(vj, vj_next);
-
+            if(j > i) {
+                delta = data.getDistance(vi_prev, vi_next) + data.getDistance(vi, vj) + data.getDistance(vi, vj_next) - data.getDistance(vi, vi_prev) - data.getDistance(vi, vi_next) - data.getDistance(vj, vj_next);
+            } else {
+                delta = data.getDistance(vi_prev, vi_next) + data.getDistance(vi, vj) + data.getDistance(vi, vj_prev) - data.getDistance(vi, vi_prev) - data.getDistance(vi, vi_next) - data.getDistance(vj, vj_prev);
+            }
+            
             if(delta < bestDelta) {
                 best_i  = i;
                 best_j = j;
                 bestDelta = delta;
-            }
+            } 
         }
         
     }
 
     //se houve redução de custo
     if(bestDelta < 0) {
-        int value = s->sequencia[best_i];
+        int value_1 = s->sequencia[best_i];
         s->sequencia.erase(s->sequencia.begin() + best_i);
-        s->sequencia.insert(s->sequencia.begin() + best_j, value);
+        s->sequencia.insert(s->sequencia.begin() + best_j, value_1);
         s->valorObj += bestDelta;
         return true;
     }
@@ -257,10 +273,10 @@ int main(int argc, char** argv) {
     cout << "Custo de S: " << cost << endl; 
 
     //cout << "CHEGUEI" << data.getDistance(1, 5) << " == " << data.getDistance(5,1) << endl;
-
+    //for(int c = 0; c < 10; c++)
     //bestImprovement2Opt(&s, data);
-    reinsertion(&s, data);
-
+    //reinsertion(&s, data, 1);
+    bestImprovementSwap(&s, data);
 
     cost = 0;
     for (size_t i = 0; i < n; i++) {
@@ -272,6 +288,9 @@ int main(int argc, char** argv) {
     //cout << n << " -> " << 1 << endl;
     cout << "Custo de S: " << s.valorObj << "===" << cost << endl;   
 
+
+
+    cout << data.getDistance(5, 5);
 
     return 0;
 }
